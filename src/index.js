3ac948d295ac792df1e0625dd6e3df2e62260271
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-06-17 21:49:17 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const stream = require('./stream.js');
+const getInfo = require('./getInfo.js');
+const getClientId = require('./getClientId.js');
+const search = require('./search.js');
+
+module.exports = { stream, getInfo, getClientId, search };

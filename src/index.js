98040e8aa66fe6d80f65c70087dbd351d65ade4b
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-06-19 14:26:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const stream = require('./stream.js');
 const getInfo = require('./getInfo.js');
 const getClientId = require('./getClientId.js');
 const search = require('./search.js');
+const { validateSoundCloudURL } = require('./util.js');
 
-module.exports = { stream, getInfo, getClientId, search };
+module.exports = { stream, getInfo, getClientId, search, validateSoundCloudURL };

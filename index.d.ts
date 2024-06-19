732d4aad5373cef04c8f896e68e0705ce85b8cb0
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2024-06-19 14:26:14 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -88,10 +88,33 @@ class SoundCloudStream extends EventEmitter{
 
 }
 
+/**
+ * Receives information about a playlist or a track on SoundCloud
+ * @param url The url of the SoundCloud playlist or track
+ */
 export function getInfo(url: string) : Promise<SoundCloudTrack | SoundCloudPlaylist>;
 
+/**
+ * Creates a readable stream for a SoundCloud track
+ * @param info The url of the SoundCloud track or an instance of the SoundCloudTrack class of the track you would like to create a readable stream for
+ * @param options Additional options for the stream function
+ */
 export function stream(info: string | SoundCloudtrack, options: StreamOptions) : Promise<SoundCloudStream>;
 
+/**
+ * Searches for a track, playlist or album on SoundCloud
+ * @param query The query you would like to use for the search
+ * @param options Additional options to specify your search
+ */
 export function search(query: string, options: SearchOptions) : Promise<[SoundCloudtrack | SoundCloudPlaylist]>;
 
+/**
+ * Updates the client id used to get the information from SoundCloud
+ */
 export function getClientId() : Promise<string>;
+
+/**
+ * Checks whether a url is a valid SoundCloud url or not
+ * @param url The SoundCloud url you would like to validate
+ */
+export function validateSoundCloudURL(url: string) : boolean;
